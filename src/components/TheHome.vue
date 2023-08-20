3c8ac342b162d12f2fafd2c672d1696f1ab351f1
Dinervc <50810420+Dinervc@users.noreply.github.com>
2023-08-20 23:28:46 +0200

    Fixed Project. Development ready now

---
+++ src/components/TheHome.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  This is a home page
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheHome from '../components/TheHome.vue'
 </script>
 
 <template>
-  <main>
-    hi
-  </main>
+  <div class="home">
+    <TheHome />
+  </div>
 </template>
+
+<style>
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+</style>
