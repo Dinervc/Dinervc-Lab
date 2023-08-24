f48b54109d856585f65d4f0db30f24f381b0c187
Dinervc <50810420+Dinervc@users.noreply.github.com>
2023-08-24 18:15:04 +0200

    Greetings text done

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,6 +1,15 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>This is a home page</h1>
+  <h1 align="center">
+    <img
+      src="https://readme-typing-svg.herokuapp.com/?font=VT323&size=35&center=true&vCenter=true&color=00bd7e&pause=2000&width=700&height=70&duration=2000&lines=Welcome!;+I'm+Dinervc,+Software+Engineer!;Creating+Innovative+Software+Solutions!;Building+the+Future+of+Tech;Turning+Visions+into+Reality" />
+  </h1>
 </template>
+
+<style>
+img {
+  height: 160px;
+  max-width: fit-content;
+}
+</style>
