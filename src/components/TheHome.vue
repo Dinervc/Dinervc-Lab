4ed4addb753baf6bab2be936841fb82815ae0501
Dinervc <50810420+Dinervc@users.noreply.github.com>
2023-08-24 22:55:14 +0200

    Alpha Version of Home Page Done

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 align="center">
+  <h1 class="typing" align="center">
     <img
-      src="https://readme-typing-svg.herokuapp.com/?font=VT323&size=35&center=true&vCenter=true&color=00bd7e&pause=2000&width=700&height=70&duration=2000&lines=Welcome!;+I'm+Dinervc,+Software+Engineer!;Creating+Innovative+Software+Solutions!;Building+the+Future+of+Tech;Turning+Visions+into+Reality" />
+      src="https://readme-typing-svg.herokuapp.com/?font=VT323&size=35&center=true&vCenter=true&color=00bd7e&pause=2000&width=700&height=70&duration=2000&lines=Welcome!;+I'm+Dinervc,+Software+Engineer!;Creating+Innovative+Software+Solutions!;Building+the+Future+of+Tech;Turning+Visions+into+Reality"
+      class="typingImg" />
   </h1>
 </template>
 
 <style>
-img {
+.typing {
+  width: 95vw;
+}
+
+.typingImg {
   height: 160px;
-  max-width: fit-content;
+  width: 95%;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,23 +14,36 @@ import TheHome from '../components/TheHome.vue'
           efficiency and modern design principles. Let's build the future of technology together.</p>
       </div>
       <div class="grid-item">
-        <h2>About This Portfolio</h2>
-        <p>This portfolio is a reflection of my journey as a Software Engineer. It showcases my skills, projects, and
+        <h2>About This Lab</h2>
+        <p>This lab is a reflection of my journey as a Software Engineer. It showcases my skills, projects, and
           passion for technology. Built with the latest web technologies, it's designed to give you insight into my
           professional capabilities.</p>
       </div>
-      <div class="grid-item">
-        <h3>Featured Project: [Project Title]</h3>
-        <p>[Project Description]</p>
-        <img src="path/to/your/image1.jpg" alt="[Image Description]">
-        <img src="path/to/your/image2.jpg" alt="[Image Description]">
-        <button><a href="path/to/demo" target="_blank">Demo</a></button>
+      <div class="grid-item" id="three">
+        <h3>Featured Project - Reflectory</h3>
+        <p>This is an AI powered mental health improvement app. Created for the Hack United hackathon in August 2023. It
+          was awarded as the best Solo Hack out of the competition.</p>
+        <br><button class="demo"><a href="path/to/demo" target="_blank">Demo</a></button>
       </div>
     </div>
   </div>
 </template>
 
 <style>
+#three {
+  background: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.7)), url('../assets/certificate.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  animation: gradient ease;
+}
+
+.demo {
+  border-radius: 5px;
+  font-size: 25px;
+  padding-bottom: 6px;
+}
+
 .grid-container {
   display: flex;
   flex-wrap: wrap;
@@ -44,5 +57,11 @@ import TheHome from '../components/TheHome.vue'
   padding: 20px;
   margin: 10px;
   border: 1px solid #ddd;
+  border-radius: 30px;
+}
+
+.grid-item>img {
+  width: 200px;
+  margin-right: 30px;
 }
 </style>
