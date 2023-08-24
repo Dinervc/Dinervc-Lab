db7ac32125f0ba34d1d2c7b1278df6b6c2597f44
Dinervc <50810420+Dinervc@users.noreply.github.com>
2023-08-24 23:19:11 +0200

    Done for now

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -3,18 +3,45 @@
 <template>
   <h1 class="typing" align="center">
     <img
-      src="https://readme-typing-svg.herokuapp.com/?font=VT323&size=35&center=true&vCenter=true&color=00bd7e&pause=2000&width=700&height=70&duration=2000&lines=Welcome!;+I'm+Dinervc,+Software+Engineer!;Creating+Innovative+Software+Solutions!;Building+the+Future+of+Tech;Turning+Visions+into+Reality"
+      src="https://readme-typing-svg.herokuapp.com/?font=VT323&size=35&center=true&vCenter=true&color=00bd7e&pause=3000&width=700&height=70&duration=3000&lines=Welcome!;+I'm+Dinervc,+Software+Engineer!;Creating+Innovative+Software+Solutions!;Building+the+Future+of+Tech;Turning+Visions+into+Reality"
       class="typingImg" />
   </h1>
 </template>
 
 <style>
-.typing {
-  width: 95vw;
+#three {
+  background: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.7)), url('../assets/certificate.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  animation: gradient ease;
 }
 
-.typingImg {
-  height: 160px;
-  width: 95%;
+.demo {
+  border-radius: 5px;
+  font-size: 25px;
+  padding-bottom: 6px;
+  background-color: black;
+}
+
+.grid-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.grid-item {
+  flex: 1 1 400px;
+  /* Adjust this value to control when items wrap */
+  box-sizing: border-box;
+  padding: 20px;
+  margin: 10px;
+  border: 1px solid #ddd;
+  border-radius: 30px;
+}
+
+.grid-item>img {
+  width: 200px;
+  margin-right: 30px;
 }
 </style>
