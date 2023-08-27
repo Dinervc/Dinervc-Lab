383846f6b48bc73b7f8ed34afb36b02d220d205d
Dinervc <50810420+Dinervc@users.noreply.github.com>
2023-08-27 18:34:10 +0200

    Nothing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,15 @@
+<script setup lang="ts">
+//import About from '../components/About.vue'
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Who am I?</h1>
   </div>
 </template>
 
 <style>
+.about {
+  margin-top: 150px;
+}
 </style>
